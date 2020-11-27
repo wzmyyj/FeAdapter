ad8ca19bc34489544ccaa26750cdb93f5bd951e8
wzmyyj <2209011667@qq.com>
2020-11-27 17:24:10 +0800

    【add】diff_api, diff_annotation, diff_compiler.

---
+++ app/src/main/java/top/wzmyyj/feadapter/D.kt
@@ -0,0 +1,13 @@
+package top.wzmyyj.feadapter
+
+/**
+ * Created on 2020/11/25.
+ *
+ * @author feling
+ * @version 1
+ * @since 1
+ */
+data class D(var a:Int?=0,var c:Boolean) {
+
+     var b:Int=0
+}
